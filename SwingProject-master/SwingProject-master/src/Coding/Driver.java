f32a51e07cb20a056a4a81d20416eb50b43f8c1e
JanakJayaweera <30425810+JanakJayaweera@users.noreply.github.com>
2017-08-19 20:44:44 +0530

    Updated color combination as per the supervisor Connected table to the database

--- SwingProject-master/SwingProject-master/src/Coding/Driver.java
+++ SwingProject-master/SwingProject-master/src/Coding/Driver.java
@@ -4,40 +4,67 @@ package Coding;
 import DB.DBconnect;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import net.proteanit.sql.DbUtils;
+import GUI.Internal.Driver.addDriver;
 
 public class Driver {
         
+        private javax.swing.JTable Table1;
+        private String fname,lname,NIC,drvlicence,DOB,telephone,address,email;
+        
         Connection con = null;
         PreparedStatement pst = null;
+        ResultSet rs = null;
         
-        String fname,lname,NIC,drvlicence,day,month,year,telephone,address,email;
         
-        public Driver(String pfname,String plname,String pNIC,String pdrvlicence,String pday,String pmonth,String pyear,String ptelephone,String paddress,String pemail)
+        public Driver(String pfname,String plname,String pNIC,String pdrvlicence,String pDOB,String ptelephone,String paddress,String pemail, javax.swing.JTable pTable1)
         {
-           con = DBconnect.connect();
+            con = DBconnect.connect();
            
-           fname = pfname;
-           lname = plname;
-           NIC = pNIC;
-           drvlicence = pdrvlicence;
-           day = pday;
-           month = pmonth;
-           year = pyear;
-           telephone = ptelephone;
-           address = paddress;
-           email = pemail;
+             fname = pfname;
+             lname = plname;
+             NIC = pNIC;
+             drvlicence = pdrvlicence;
+             DOB = pDOB;
+             telephone = ptelephone;
+             address = paddress;
+             email = pemail;
+             Table1 = pTable1;
            
-           try 
-        {
-        String q = "INSERT INTO driver (fname,lname,nic,licenceno,birthdate,birthmonth,birthyear,telephone,address,email) values ('"+ fname +"','"+ lname +"','"+ NIC +"','"+ drvlicence +"','"+ day +"','"+ month +"','"+ year +"','"+ telephone +"','"+ address +"','"+ email +"')";
-        pst = con.prepareStatement(q);
-        pst.execute();
-        } 
-        
-        catch (SQLException e) {
-            
+             addDriver();
         }
-                 
+        
+        
+        public void addDriver(){   
+           try 
+            {
+                String q = "INSERT INTO driver (fname,lname,nic,licenceno,DOB,telephone,address,email) values ('"+ fname +"','"+ lname +"','"+ NIC +"','"+ drvlicence +"','"+ DOB +"','"+ telephone +"','"+ address +"','"+ email +"')";
+                pst = con.prepareStatement(q);
+                pst.execute();
+                tableLoad();
+            } 
+        
+            catch (SQLException e) {
+                System.out.println(e);
+            }
+        }        
+        
+        
+        public void tableLoad()
+        {           
+            
+            try{
+            
+                String sql = "SELECT driverID,fname,lname,nic,licenceno,DOB,telephone,address,email FROM driver";
+                pst = con.prepareStatement(sql);
+                rs = pst.executeQuery();
+                Table1.setModel(DbUtils.resultSetToTableModel(rs));
+            }
+            
+            catch(SQLException e){
+                System.out.println(e);
+            }
         }
 }
